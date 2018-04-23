eff070cc2717000ed8753808ee8efb9314b87e87
ZalkinV <Zalkin.viktor@mail.ru>
2018-04-23 22:11:10 +0300

    Add ZeroDivision checking to functions
    В фнукциях где может произойти деление на 0, добавил проверку на это. Если такое случается (обычно, если погрешность (error) равна 0), то выводится сообщение и используется в качестве корня последнее вычисленное значение

--- Lab3/Lab3/Lab3.py
+++ Lab3/Lab3/Lab3.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 helpLineWidth = 0.9
+maxMethodIterations = 15
 
 def Function(x):
 	#return np.cos(x) - 6 * x + 1
@@ -57,14 +58,19 @@ def DrawMainGraph(begin, end, figure, precision = 100):
 	pass
 
 def DrawSecantLine(x0, x1, figure = -1):
-	#TODO: Надо добавить проверку деления на ноль. Можно в этом случае просто сдвигать точки и сообщать (или нет) об этом пользователю
-	x2 = x1 - Function(x1) / ApproxDerivative(x0, x1)
-
-	if (figure != -1):
-		plt.figure(figure)
-		plt.plot([x0, x0], [Function(x0), 0], 'g--', linewidth=helpLineWidth)
-		plt.plot([x1, x1], [Function(x1), 0], 'g--', linewidth=helpLineWidth)
-		plt.plot([x0, x1, x2], [Function(x0), Function(x1), 0], 'g-', linewidth=helpLineWidth)
+	#TODO: Надо добавить проверку деления на ноль.  Можно в этом случае просто
+	#сдвигать точки и сообщать (или нет) об этом пользователю
+	try:
+		x2 = x1 - Function(x1) / ApproxDerivative(x0, x1)
+	
+		if (figure != -1):
+			plt.figure(figure)
+			plt.plot([x0, x0], [Function(x0), 0], 'g--', linewidth=helpLineWidth)
+			plt.plot([x1, x1], [Function(x1), 0], 'g--', linewidth=helpLineWidth)
+			plt.plot([x0, x1, x2], [Function(x0), Function(x1), 0], 'g-', linewidth=helpLineWidth)
+	except ZeroDivisionError:
+		print("Производная в точке {x} равна 0. Досрочно прекращаю вычисления.".format(x=x0))
+		raise ZeroDivisionError
 
 	return x2
 
@@ -73,23 +79,32 @@ def MethodSecant(x0, x1, error, figure = -1):
 	xCurrent = x1
 	yNext = Function(x1)
 	i = 0
+	
+	try:
+		while (abs(yNext) >= error and i < maxMethodIterations):
+			xNext = DrawSecantLine(xPrevious, xCurrent, figure)
+			yNext = Function(xNext)
+			xPrevious = xCurrent
+			xCurrent = xNext
+			i += 1
+	except ZeroDivisionError:
+		pass
 
-	while (abs(yNext) > error and i < 100):
-		xNext = DrawSecantLine(xPrevious, xCurrent, figure)
-		yNext = Function(xNext)
-		xPrevious = xCurrent
-		xCurrent = xNext
-		i += 1
 	return xCurrent
 
 def DrawTangentLine(x0, figure = -1):
-		#TODO: Надо добавить проверку деления на ноль. Можно в этом случае просто сдвигать точки и сообщать (или нет) об этом пользователю
-	x1 = x0 - Function(x0) / Derivative(x0)
-
-	if (figure != -1):
-		plt.figure(figure)
-		plt.plot([x0, x0], [Function(x0), 0], 'r--', linewidth=helpLineWidth)
-		plt.plot([x0, x1], [Function(x0), 0], 'r-', linewidth=helpLineWidth)
+	#TODO: Надо добавить проверку деления на ноль.  Можно в этом случае просто
+	#сдвигать точки и сообщать (или нет) об этом пользователю
+	try:
+		x1 = x0 - Function(x0) / Derivative(x0)
+		
+		if (figure != -1):
+			plt.figure(figure)
+			plt.plot([x0, x0], [Function(x0), 0], 'r--', linewidth=helpLineWidth)
+			plt.plot([x0, x1], [Function(x0), 0], 'r-', linewidth=helpLineWidth)
+	except ZeroDivisionError:
+		print("Производная в точке {x} равна 0. Досрочно прекращаю вычисления.".format(x=x0))
+		raise ZeroDivisionError
 
 	return x1
 
@@ -98,24 +113,32 @@ def MethodTangent(x0, error, figure = -1):
 	yCurrent = Function(x0)
 	i = 0
 	
-	while (abs(yCurrent) > error and i < 100):
-		xCurrent = DrawTangentLine(xCurrent, figure)
-		yCurrent = Function(xCurrent)
-		i += 1
+	try:
+		while (abs(yCurrent) >= error and i < maxMethodIterations):
+			xCurrent = DrawTangentLine(xCurrent, figure)
+			yCurrent = Function(xCurrent)
+			i += 1
+	except ZeroDivisionError:
+		pass
+
 	return xCurrent
 
 def GetCalcPrecision(error):
-	precisionCalc = 100
-	digitNumber = -2
-	while (error % precisionCalc >= error):
-		digitNumber += 1
-		precisionCalc /= 10
-	return digitNumber
+	precisionCalc = 1
+	digitalNumber = 0
+	try:
+		while (error % precisionCalc >= error):
+			digitalNumber += 1
+			precisionCalc /= 10
+	except ZeroDivisionError:
+		pass
+
+	return digitalNumber
 
 
 def Main():
 	precision = 100
-	error = 0.05
+	error = 0
 	bounds = GetInputData()
 	print("Поиск корней на промежутке [{a}; {b}] ...".format(a=bounds[0], b=bounds[1]))
 	
@@ -127,8 +150,8 @@ def Main():
 	plt.plot(rootSec, Function(rootSec), 'og')
 	plt.show()
 
-	print("Метод Ньютона(касательных): ", round(rootTan, GetCalcPrecision(error)))
-	print("Метод Секущих: ", round(rootSec, GetCalcPrecision(error)))
+	print("Метод Ньютона(касательных): x =", round(rootTan, GetCalcPrecision(error)))
+	print("Метод Секущих: x =", round(rootSec, GetCalcPrecision(error)))
 	pass
 
 Main()
